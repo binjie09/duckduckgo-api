f36bc6275ba5d9e2eb270d3ce33f549397a1c05a
陈彬杰 <binjie.chen@zknow.com>
2023-03-20 16:32:48 +0800

    add simple text search api

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # duckduckgo-api
+
+```bash
+python3 -m venv myenv && source myenv/bin/activate && pip install -r requirements.txt
+
+```
---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/search')
+def hello_world():  # put application's code here
+    from duckduckgo_search import ddg
+
+    keywords = request.args.get('q')
+    print(request.args.get('max_results'))
+    max_results = request.args.get('max_results') or 3
+    results = ddg(keywords, region='wt-wt', safesearch='Off', time='y', max_results=max_results)
+    print(results)
+    return results
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
