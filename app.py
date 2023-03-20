2577071a7902ad5a0dcf39f622a49070a21c95d1
陈彬杰 <binjie.chen@zknow.com>
2023-03-20 17:03:44 +0800

    params update

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def hello_world():  # put application's code here
     keywords = request.args.get('q')
     print(request.args.get('max_results'))
     max_results = int(request.args.get('max_results')) or 3
-    results = ddg(keywords, region='wt-wt', safesearch='Off', time='y', max_results=max_results)
+    results = ddg(keywords, region='wt-wt', max_results=max_results)
     print(results)
     return results
 
